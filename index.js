d20fda219f9c6165b027abe22bc28c04765ebc0c
Addison Higham <addisonj@gmail.com>
2014-06-12 23:00:23 -0600

    it kinda works

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 var stompServer = require("blackcatmq")
+var BacksideProxy = require("backside-proxy")
+var MemoryStore = require("backside-memory-store")
+var container = BacksideProxy.getContainer()
 
-var confFile = process.argv[2]
-var conf = {
-  STOMP_HOST: process.env["STOMP_HOST"] || "localhost",
-  STOMP_PORT: process.env["STOMP_PORT"] || 61613,
+container.register("store", function() {
+  return new MemoryStore()
+  console.log("stuff", MemoryStore)
+  return store
+})
 
-}
+var WS_PORT = container.get("WS_PORT")
+var TCP_PORT = container.get("TCP_PORT")
+var API_PORT = container.get("API_PORT")
 
-if (confFile) {
-  conf = JSON.parse(fs.readFileSync(confFile))
-}
-var api = require("backside-api")(conf)
-var proxy = require("backside-proxy")(conf)
+var proxyServers = BacksideProxy.createServers()
+var apiServer = container.get("server")
 
-var proxyServers = proxy.createServers()
+proxyServers.ws.listen(WS_PORT, function() {
+  console.log("started stomp websocket proxy on port " + WS_PORT)
+})
+proxyServers.tcp.listen(TCP_PORT, function() {
+  console.log("started stomp tcp proxy on port " + TCP_PORT)
+})
+apiServer.listen(API_PORT, function() {
+  console.log("started api on port " + API_PORT)
+})
 
-stompServer.create(
+
+
 
 
